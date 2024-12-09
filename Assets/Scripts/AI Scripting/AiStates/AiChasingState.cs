d25f04cd4fa7df85170bfe6b1bd2374cec17fb2b
Marr10tt <Mackenziejmarriott@gmail.com>
2024-12-09 03:32:50 +0000

    AI Waypoint System
    AIs now use a waypoint system to navigate between places on the map

--- Assets/Scripts/AI Scripting/AiStates/AiChasingState.cs
+++ Assets/Scripts/AI Scripting/AiStates/AiChasingState.cs
@@ -1,9 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 public class AiChasingState : AiState
 {
+
+    int waypointIndex = 0;
     public AiStateId GetId()
     {
         return AiStateId.Chasing;
@@ -12,14 +11,25 @@ public class AiChasingState : AiState
     public void Enter(AiAgent agent)
     {
         agent.animator.SetFloat("Speed", 2);
+        agent.navMeshAgent.SetDestination(agent.waypoints[0].position);
     }
 
     public void Update(AiAgent agent)
     {
-        agent.navMeshAgent.SetDestination(agent.waypoints[0].position);
 
         //change animation based on distance to finish point, if not moving, stop walk animation
-        if(agent.navMeshAgent.velocity.magnitude <= 0.15f){
+        if(Vector3.Distance(agent.waypoints[waypointIndex].position, agent.transform.position) <=3){
+            if(waypointIndex>=agent.waypoints.Count-1){
+                waypointIndex = 0;
+
+            }
+            else if (agent.waypoints.Count==1){
+                agent.stateMachine.ChangeState(AiStateId.Idle);
+            }
+            else{
+                waypointIndex++;
+            }
+            agent.navMeshAgent.SetDestination(agent.waypoints[waypointIndex].position);
             agent.animator.SetFloat("Speed", 0);
         }
         else{
